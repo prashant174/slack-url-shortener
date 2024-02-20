5767d883509ad8e240ee2ce4b00c9daf5fe841aa
Prashant Kumar Yadav <yprashant174@gmail.com>
2024-02-20 18:47:51 +0530

    Update urlModel.js

--- models/urlModel.js
+++ models/urlModel.js
@@ -1,17 +1,9 @@
 const mongoose=require("mongoose")
 
 const urlSchema=new mongoose.Schema({
-    shortId:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    redirectUrl:{
-        type:String,
-        required:true
-    },
-    visitHistory:[{timestamp:{type:Number}}],
-},{timestamps:true})
+    longUrl: { type: String, required: true },
+  shortUrl: { type: String, required: true, unique: true },
+})
 
 const URL=mongoose.model('url',urlSchema)
 
