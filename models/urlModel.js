ac6882d2fcd1fdc2b297fecd88eb12d893cadb78
Prashant Kumar Yadav <yprashant174@gmail.com>
2024-02-22 02:32:01 +0530

    visithistory add in schema

--- models/urlModel.js
+++ models/urlModel.js
@@ -1,9 +1,17 @@
 const mongoose=require("mongoose")
 
 const urlSchema=new mongoose.Schema({
-    longUrl: { type: String, required: true },
-  shortUrl: { type: String, required: true, unique: true },
-})
+    shortId:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    redirectUrl:{
+        type:String,
+        required:true
+    },
+    visitHistory:[{timestamp:{type:Number}}],
+},{timestamps:true})
 
 const URL=mongoose.model('url',urlSchema)
 
