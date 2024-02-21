9dbd9a04463d16ced144be480dc042c21c4c5b36
Prashant Kumar Yadav <yprashant174@gmail.com>
2024-02-22 02:31:17 +0530

    visithistory added in url analytics

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -2,21 +2,33 @@ const shortid=require("shortid")
 const { URL } = require("../models/urlModel")
 
 const generateNewUrl= async(req,res)=>{
-  const { longUrl } = req.body;
-  const shortUrl = shortid();
-  await URL.create({ longUrl, shortUrl });
-  res.json({ shortUrl });
+    const body=req.body
+    if(!body.url){
+        return res.status(400).send({msg:"Url is required"})
+    }
+  const shortId=shortid()
 
+  await URL.create({
+    shortId:shortId,
+     redirectUrl:body.url,
+     visitHistory:[]
+  });
+
+  return res.json({id:shortId})
 }
 
 const shortUrl=async(req,res)=>{
     try {
-       const { shortUrl } = req.params;
-  const url = await URL.findOne({ shortUrl });
-  if (!url) {
-    return res.status(404).json({ error: 'Short URL not found' });
-  }
-  res.redirect(url.longUrl);
+        const shortId=req.params.shortId;
+        const entry=await URL.findOneAndUpdate({
+            shortId
+        },{$push:{
+            visitHistory:{
+                timestamp:Date.now()
+            },
+        }})
+    
+        res.redirect(entry.redirectUrl)
     } catch (error) {
         console.log(error)
     }
